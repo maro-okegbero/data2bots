db704bb9805fbad7dd5d79ba3bb2677a9a29b7e9
maro <marookegbero@gmail.com>
2022-02-05 07:23:50 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 from sniffer import sniffer
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+# Press the green button in the gutter to run the script if you are on pycharm
 if __name__ == '__main__':
-    sniffer()
-    print_hi('PyCharm')
+    sniffer(file=None)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
